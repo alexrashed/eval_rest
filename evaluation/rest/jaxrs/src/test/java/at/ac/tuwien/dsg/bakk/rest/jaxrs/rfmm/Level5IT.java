0bbcc4e20ff5b759a3df7c642cdf1a8387557741
alexrashed <alexander.rashed@gmail.com>
2016-06-24 12:42:26 +0200

    Tests for RFMM Level 5

---
+++ evaluation/rest/jaxrs/src/test/java/at/ac/tuwien/dsg/bakk/rest/jaxrs/rfmm/Level5IT.java
@@ -0,0 +1,19 @@
+package at.ac.tuwien.dsg.bakk.rest.jaxrs.rfmm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Integration test executing the tests for Level 5 of the REST Framework
+ * Maturity Model.
+ * 
+ * @author Alexander Rashed, 1325897, alexander.rashed@tuwien.ac.at
+ */
+public class Level5IT {
+
+	@Test
+	public void testLevel5_1_LinkTraversion() {
+		Assert.fail("No client-side link traversion API provided");
+	}
+
+}
